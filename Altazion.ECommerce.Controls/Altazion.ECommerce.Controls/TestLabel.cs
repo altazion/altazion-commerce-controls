c859d9054f38e2c64cc387079ad37926f6338e35
Michael CARBENAY <michael@commerce-hub.io>
2019-07-06 09:35:28 +0200

    Création du projet

---
+++ Altazion.ECommerce.Controls/Altazion.ECommerce.Controls/TestLabel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.UI.WebControls;
+
+namespace Altazion.ECommerce.Controls
+{
+    public class TestLabel : Label
+    {
+        protected override void OnPreRender(EventArgs e)
+        {
+            base.OnPreRender(e);
+            this.Text = "ceci est un test coté github.";
+        }
+    }
+}
